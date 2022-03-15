df2d98eb9bbf7bff03e35b3c536d87dea667ebce
Joel Murtaugh <murtaugh05@gmail.com>
2022-03-14 20:06:33 -0400

    Finished quiz section, maybe clean it up a bit. Added the initialize function. Clean up code for next time.

--- Application/Program.cs
+++ Application/Program.cs
@@ -7,12 +7,11 @@ namespace musicTheoryApp
     {
         static void Main(string[] args)
         {
-            List<Questions> questionsList = new List<Questions>();
+            List<Questions> questionsList = Questions.Initialize();
 
-            Questions.Initialize(); 
+            Console.WriteLine(questionsList);
 
-
-            Questions question1 = new Questions
+            /*Questions question1 = new Questions
             {
                 QuestionNumber = 1,
                 Question = "Is this major or minor??\nC D E F G A B C",
@@ -47,11 +46,13 @@ namespace musicTheoryApp
                 Answer = "Major"
             };
 
-            questionsList.Add(question1);
+            questionsList.Add(Questions.Initialize();
             questionsList.Add(question2);
             questionsList.Add(question3);
             questionsList.Add(question4);
-            questionsList.Add(question5);
+            questionsList.Add(question5);*/
+
+            int correctAnswers = 0;
 
             bool runLoop = true;
             while(runLoop)
@@ -73,11 +74,109 @@ namespace musicTheoryApp
                         break;
 
                     case 2:
-                        Console.WriteLine("quiz");
+                        Console.Clear();
+                        Console.WriteLine("QUIZ");
+
+                        int i = 0;
+                        while ( i<questionsList.Count)
+                        {
+                            Console.WriteLine("Question " + questionsList[i].QuestionNumber);
+                            Console.WriteLine(questionsList[i].Question);
+                            if (questionsList[i].Answer.ToLower() != Console.ReadLine())
+                            {
+                                Console.WriteLine("Incorrect");
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                correctAnswers++;
+                                Console.WriteLine("Correct!");
+                                Console.WriteLine();
+                            }
+
+                            i++;
+                        }
+
+                        /*Console.WriteLine("Question: " + questionsList[0].QuestionNumber);
+                        Console.WriteLine(question1.Question);
+                        string answerOne = Console.ReadLine();
+                        if(question1.Answer.ToLower() != answerOne.ToLower())
+                        {
+                            Console.WriteLine("Incorrect");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            correctAnswers++; 
+                            Console.WriteLine("Correct!");
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("Question: " + question2.QuestionNumber);
+                        Console.WriteLine(question2.Question);
+                        string answerTwo = Console.ReadLine();
+                        if (question2.Answer.ToLower() != answerTwo.ToLower())
+                        {
+                            Console.WriteLine("Incorrect");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            correctAnswers++;
+                            Console.WriteLine("Correct!");
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("Question: " + question3.QuestionNumber);
+                        Console.WriteLine(question3.Question);
+                        string answerThree = Console.ReadLine();
+                        if (question3.Answer.ToLower() != answerThree.ToLower())
+                        {
+                            Console.WriteLine("Incorrect");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            correctAnswers++;
+                            Console.WriteLine("Correct!");
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("Question: " + question4.QuestionNumber);
+                        Console.WriteLine(question4.Question);
+                        string answerFour = Console.ReadLine();
+                        if (question4.Answer.ToLower() != answerFour.ToLower())
+                        {
+                            Console.WriteLine("Incorrect");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            correctAnswers++;
+                            Console.WriteLine("Correct!");
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("Question: " + question5.QuestionNumber);
+                        Console.WriteLine(question5.Question);
+                        string answerFive = Console.ReadLine();
+                        if (question5.Answer.ToLower() != answerFive.ToLower())
+                        {
+                            Console.WriteLine("Incorrect");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            correctAnswers++;
+                            Console.WriteLine("Correct!");
+                            Console.WriteLine();
+                        }*/
+
                         break;
 
                     case 3:
                         Console.WriteLine("quiz results");
+                        Console.WriteLine(correctAnswers + " out of 5");
                         break;
 
                     case 4:
@@ -87,12 +186,10 @@ namespace musicTheoryApp
 
                 Console.WriteLine("back to menu? yes or no");
                 string loopOrExit = Console.ReadLine();
-
                 if (loopOrExit == "no")
                 {
                     runLoop = false;
                 }
-
             }
         }
 
@@ -102,14 +199,51 @@ namespace musicTheoryApp
             public string Question { get; set; }
             public string Answer { get; set; }
 
-            public static void Initialize()
+            public static List<Questions> Initialize()
             {
+                List<Questions> questionsList = new List<Questions>();
+
                 Questions question1 = new Questions
                 {
                     QuestionNumber = 1,
                     Question = "Is this major or minor??\nC D E F G A B C",
                     Answer = "Major"
                 };
+                questionsList.Add(question1);
+
+                Questions question2 = new Questions
+                {
+                    QuestionNumber = 2,
+                    Question = "Is this major or minor??\nA B C D E F G A",
+                    Answer = "Minor"
+                };
+                questionsList.Add(question2);
+
+                Questions question3 = new Questions
+                {
+                    QuestionNumber = 3,
+                    Question = "Is this major or minor??\nEb F G Ab Bb C D Eb",
+                    Answer = "Major"
+                };
+                questionsList.Add(question3);
+
+                Questions question4 = new Questions
+                {
+                    QuestionNumber = 4,
+                    Question = "Is this major or minor??\nC D Eb F G Ab Bb C",
+                    Answer = "Minor"
+                };
+                questionsList.Add(question4);
+
+                Questions question5 = new Questions
+                {
+                    QuestionNumber = 5,
+                    Question = "Bonus Question! Is this major or minor??\nF G A Bb C D E F",
+                    Answer = "Major"
+                };
+                questionsList.Add(question5);
+
+                return questionsList;
             }
         }
     }
