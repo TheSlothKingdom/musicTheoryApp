724a3474c5f962559ea6c61a692907ac84c86c58
Joel Murtaugh <murtaugh05@gmail.com>
2022-03-21 19:44:15 -0400

    Finished the quiz results area.

--- Application/Program.cs
+++ Application/Program.cs
@@ -9,50 +9,8 @@ namespace musicTheoryApp
         {
             List<Questions> questionsList = Questions.Initialize();
 
-            Console.WriteLine(questionsList);
-
-            /*Questions question1 = new Questions
-            {
-                QuestionNumber = 1,
-                Question = "Is this major or minor??\nC D E F G A B C",
-                Answer = "Major"
-            };
-
-            Questions question2 = new Questions
-            {
-                QuestionNumber = 2,
-                Question = "Is this major or minor??\nA B C D E F G A",
-                Answer = "Minor"
-            };
-
-            Questions question3 = new Questions
-            {
-                QuestionNumber = 3,
-                Question = "Is this major or minor??\nEb F G Ab Bb C D Eb",
-                Answer = "Major"
-            };
-
-            Questions question4 = new Questions
-            {
-                QuestionNumber = 4,
-                Question = "Is this major or minor??\nC D Eb F G Ab Bb C",
-                Answer = "Minor"
-            };
-
-            Questions question5 = new Questions
-            {
-                QuestionNumber = 5,
-                Question = "Bonus Question! Is this major or minor??\nF G A Bb C D E F",
-                Answer = "Major"
-            };
-
-            questionsList.Add(Questions.Initialize();
-            questionsList.Add(question2);
-            questionsList.Add(question3);
-            questionsList.Add(question4);
-            questionsList.Add(question5);*/
-
             int correctAnswers = 0;
+            int totalQuestions = 0;
 
             bool runLoop = true;
             while(runLoop)
@@ -78,7 +36,7 @@ namespace musicTheoryApp
                         Console.WriteLine("QUIZ");
 
                         int i = 0;
-                        while ( i<questionsList.Count)
+                        while (i < questionsList.Count)
                         {
                             Console.WriteLine("Question " + questionsList[i].QuestionNumber);
                             Console.WriteLine(questionsList[i].Question);
@@ -96,87 +54,17 @@ namespace musicTheoryApp
 
                             i++;
                         }
-
-                        /*Console.WriteLine("Question: " + questionsList[0].QuestionNumber);
-                        Console.WriteLine(question1.Question);
-                        string answerOne = Console.ReadLine();
-                        if(question1.Answer.ToLower() != answerOne.ToLower())
-                        {
-                            Console.WriteLine("Incorrect");
-                            Console.WriteLine();
-                        }
-                        else
-                        {
-                            correctAnswers++; 
-                            Console.WriteLine("Correct!");
-                            Console.WriteLine();
-                        }
-
-                        Console.WriteLine("Question: " + question2.QuestionNumber);
-                        Console.WriteLine(question2.Question);
-                        string answerTwo = Console.ReadLine();
-                        if (question2.Answer.ToLower() != answerTwo.ToLower())
-                        {
-                            Console.WriteLine("Incorrect");
-                            Console.WriteLine();
-                        }
-                        else
-                        {
-                            correctAnswers++;
-                            Console.WriteLine("Correct!");
-                            Console.WriteLine();
-                        }
-
-                        Console.WriteLine("Question: " + question3.QuestionNumber);
-                        Console.WriteLine(question3.Question);
-                        string answerThree = Console.ReadLine();
-                        if (question3.Answer.ToLower() != answerThree.ToLower())
-                        {
-                            Console.WriteLine("Incorrect");
-                            Console.WriteLine();
-                        }
-                        else
-                        {
-                            correctAnswers++;
-                            Console.WriteLine("Correct!");
-                            Console.WriteLine();
-                        }
-
-                        Console.WriteLine("Question: " + question4.QuestionNumber);
-                        Console.WriteLine(question4.Question);
-                        string answerFour = Console.ReadLine();
-                        if (question4.Answer.ToLower() != answerFour.ToLower())
-                        {
-                            Console.WriteLine("Incorrect");
-                            Console.WriteLine();
-                        }
-                        else
-                        {
-                            correctAnswers++;
-                            Console.WriteLine("Correct!");
-                            Console.WriteLine();
-                        }
-
-                        Console.WriteLine("Question: " + question5.QuestionNumber);
-                        Console.WriteLine(question5.Question);
-                        string answerFive = Console.ReadLine();
-                        if (question5.Answer.ToLower() != answerFive.ToLower())
-                        {
-                            Console.WriteLine("Incorrect");
-                            Console.WriteLine();
-                        }
-                        else
-                        {
-                            correctAnswers++;
-                            Console.WriteLine("Correct!");
-                            Console.WriteLine();
-                        }*/
+                        totalQuestions += 5;
 
                         break;
 
                     case 3:
+                        Console.Clear();
                         Console.WriteLine("quiz results");
-                        Console.WriteLine(correctAnswers + " out of 5");
+                        Console.WriteLine(correctAnswers + " out of " + totalQuestions);
+                        double displayPercentage = Questions.CalculatePercentageCorrect(correctAnswers, totalQuestions);
+                        Console.WriteLine((displayPercentage * 100).ToString("##.##") + "%");
+
                         break;
 
                     case 4:
@@ -189,7 +77,7 @@ namespace musicTheoryApp
                 if (loopOrExit == "no")
                 {
                     runLoop = false;
-                }
+                } 
             }
         }
 
@@ -244,6 +132,20 @@ namespace musicTheoryApp
                 questionsList.Add(question5);
 
                 return questionsList;
+            }
+
+            public static double CalculatePercentageCorrect(int correctAnswers, int totalQuestions)
+            {
+                if (totalQuestions != 0)
+                {
+                    double percentageCorrect = Convert.ToDouble(correctAnswers) / Convert.ToDouble(totalQuestions);
+                    return percentageCorrect;
+
+                }
+                else
+                {
+                    return 0;
+                }
             }
         }
     }
